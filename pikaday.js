3931dbb6aa738a6e402173772872179b5ecb95d0
Jesse Yang <jesse.yang@booking.com>
2015-09-01 18:51:30 +0200

    Pass the pikaday object as `this` context for onDraw

--- pikaday.js
+++ pikaday.js
@@ -1028,7 +1028,7 @@
             }
 
             if (typeof this._o.onDraw === 'function') {
-                this._o.onDraw(this);
+                this._o.onDraw.call(this);
             }
           // let the screen reader user know to use arrow keys
           this._o.field.setAttribute('aria-label', 'Use the arrow keys to pick a date');
